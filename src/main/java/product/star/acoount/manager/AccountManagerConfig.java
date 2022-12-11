57c2be1b3b1fc6d5bb3d820f6d944c00699df870
Anton <hacerock2356@gmail.com>
2022-12-11 14:04:42 +0300

    New Worked Project for transfer money

---
+++ src/main/java/product/star/acoount/manager/AccountManagerConfig.java
@@ -0,0 +1,11 @@
+package product.star.acoount.manager;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Configuration
+@ComponentScan("product.star.acoount.manager")
+public class AccountManagerConfig {
+
+}
---
+++ src/main/java/product/star/acoount/manager/AccountService.java
@@ -0,0 +1,6 @@
+package product.star.acoount.manager;
+
+public interface AccountService {
+    void transfer(Account from,Account to, long amount);
+    void transferByPhoneNumber(Account from,String phoneNumber,long amount);
+}
